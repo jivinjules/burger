96d8205e82f036fd9a68a5c1fcdc0fd552fe94a3
jivinjules <juliehodges05@gmail.com>
2018-09-15 15:31:15 -0500

    created format

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Kimchi Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portabello Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef on Weck', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
