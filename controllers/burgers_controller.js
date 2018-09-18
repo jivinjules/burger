d2bbfab1fb504d7ff172bb38a2fbdeefe55078fa
jivinjules <juliehodges05@gmail.com>
2018-09-18 08:33:57 -0500

    file updates

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,56 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+//creates the routes
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+
+        var handlebarsobject = {
+            burgers: data
+        };
+        console.log(handlebarsobject);
+        res.render("index", handlebarsobject)
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+            req.body.burger_name, req.body.devoured
+        ], function (result) {
+            //send back the id of the new burger
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+////////////////////////// CHECK THIS
+app.use("/", routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
